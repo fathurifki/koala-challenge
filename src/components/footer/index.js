9910fe2135e8d6f8599287433bf9b80452ead0b5
fathurifki <rifkifathu@gmail.com>
2020-12-03 10:38:42 +0700

    fixing transition

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -13,11 +13,11 @@ const Footer = () => {
     return (
         <React.Fragment>
             <div className="border-t flex flex-row bg-white w-full h-16 justify-center items-center shadow-x-md">
-                <div onClick={() => stateHistory('palindrome')} className="flex rounded-md w-1/2 flex-col justify-center items-center p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                <div onClick={() => stateHistory('palindrome')} className="flex rounded-md w-1/2 flex-col justify-center items-center p-2 transition duration-500 ease-in-out hover:shadow-lg">
                     <span>🖊️</span>
                     <span> Palindrome </span>
                 </div>
-                <div onClick={() => stateHistory('currency')} className="flex rounded-md w-1/2 flex-col justify-center items-center p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                <div onClick={() => stateHistory('currency')} className="flex rounded-md w-1/2 flex-col justify-center items-center p-2 transition duration-500 ease-in-out hover:shadow-lg">
                     <span>💲</span>
                     <span> Currency </span>
                 </div>
